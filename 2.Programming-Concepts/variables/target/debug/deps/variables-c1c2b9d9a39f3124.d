b06f6590cef4922b8d8c246235b5d55ccaee758d
kabeerx9 <kabeer786joshi@gmail.com>
2023-12-13 00:25:09 +0530

    Using cargo

---
+++ 2.Programming-Concepts/variables/target/debug/deps/variables-c1c2b9d9a39f3124.d
@@ -0,0 +1,5 @@
+/home/kabeerx9/personal/RUST/learning-book/2.Programming-Concepts/variables/target/debug/deps/variables-c1c2b9d9a39f3124: src/main.rs
+
+/home/kabeerx9/personal/RUST/learning-book/2.Programming-Concepts/variables/target/debug/deps/variables-c1c2b9d9a39f3124.d: src/main.rs
+
+src/main.rs:
---
+++ 2.Programming-Concepts/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/kabeerx9/personal/RUST/learning-book/2.Programming-Concepts/variables/target/debug/variables: /home/kabeerx9/personal/RUST/learning-book/2.Programming-Concepts/variables/src/main.rs
